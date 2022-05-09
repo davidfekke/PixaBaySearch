3065dd139a974842128ef3383314d5708ce73fc7
David Fekke <david@fekke.com>
2022-05-09 12:19:40 -0400

    Initial Commit

---
+++ PixaBaySearch/PixaBaySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixaBaySearchApp.swift
+//  PixaBaySearch
+//
+//  Created by David Fekke on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PixaBaySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
